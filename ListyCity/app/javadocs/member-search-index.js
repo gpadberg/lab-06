561e443b7b6c7a78512507394495174f0915811d
Grace Padberg <gpadberg@ualberta.ca>
2025-10-17 12:33:24 -0600

    generated final javadocs

---
+++ ListyCity/app/javadocs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"com.example.listycity","c":"ExampleUnitTest","l":"addition_isCorrect()"},{"p":"com.example.listycity","c":"CityListTest","l":"CityListTest()","u":"%3Cinit%3E()"},{"p":"com.example.listycity","c":"ExampleUnitTest","l":"ExampleUnitTest()","u":"%3Cinit%3E()"}];updateSearchResults();
--- ListyCity/app/src/main/java/com/example/listycity/City.java
+++ ListyCity/app/src/main/java/com/example/listycity/City.java
@@ -18,27 +18,59 @@ public class City implements Comparable<City> {
         this.province = province;
     }
 
+    /**
+     * This returns the name of the city
+     * @return
+     *  Return the name of the city
+     */
     public String getCityName() {
         return city;
     }
 
+    /**
+     * This sets the name of the city
+     * @param city
+     *  This is the name of the city to set
+     */
     public void setCity(String city) {
         this.city = city;
     }
 
+    /**
+     * This returns the name of the province
+     * @return
+     *  Return the name of the province
+     */
     public String getProvinceName() {
         return province;
     }
 
+    /**
+     * This sets the name of the province
+     * @param province
+     *  This is the name of the province to set
+     */
     public void setProvince(String province) {
         this.province = province;
     }
 
+    /**
+     * This compares two cities based on their names
+     * @param c the object to be compared.
+     * @return
+     *  Return 0 if the names are the same
+     */
     @Override
     public int compareTo(City c) {
         return this.city.compareTo(c.getCityName());
     }
 
+    /**
+     * This returns whether two cities are equal or not, based on their city and province names
+     * @param o
+     * @return
+     *  Return true if the cities are equal, false otherwise
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -47,6 +79,11 @@ public class City implements Comparable<City> {
         return city.equals(city1.getCityName()) && province.equals(city1.getProvinceName());
     }
 
+    /**
+     * This returns the hashcode of the city
+     * @return
+     *  Return the hashcode
+     */
     @Override
     public int hashCode() {
         return Objects.hash(city, province);
